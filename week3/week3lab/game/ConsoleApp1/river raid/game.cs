eb972d2a249ce8b7d771799f3e7d47607fe10d25
noorulhuda789 <120157690+noorulhuda789@users.noreply.github.com>
2023-05-02 00:04:40 +0500

    Add files via upload

---
+++ week3/week3lab/game/ConsoleApp1/river raid/game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game.river_raid
+{
+    class Game
+    {
+        public int score;
+        public int health;
+        public int heroX;
+        public int heroY;
+        public int herox;
+        public int heroy;
+        public int bulletcount = 0, bulletenmeyX, bulletenemyY, count, bulltmovey, bulltmovex;
+        public Game(int score, int health, int heroX, int heroY, int herox, int heroy)
+        {
+            this.score = score;
+            this.health = health;
+            this.heroX = heroX;
+            this.heroY = heroY;
+            this.herox = herox;
+            this.heroy = heroy;
+            int bulletcount = 0, bulletenmeyX, bulletenemyY, count, bulltmovey, bulltmovex;
+        }
+    }
+}
