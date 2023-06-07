17f729de949d9382cc393d9dcffb5de55107e37e
noorulhuda789 <120157690+noorulhuda789@users.noreply.github.com>
2023-06-07 13:56:11 +0500

    Add files via upload

---
+++ week6Pd/Pacman/Pacman/BL/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman.BL
+{
+    class Cell
+    {
+        public char value;
+        public int x;
+        public int y;
+    }
+}
---
+++ week6Pd/Pacman/Pacman/BL/Grid.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace Pacman.BL
+{
+    class Grid
+    {
+        public char[,] maze ;
+        public int rowsize;
+        public int columnsize;
+        public Grid(string path,int rowsize,int columnsize)
+        {
+            this.rowsize = rowsize;
+            this.columnsize = columnsize;
+            int row = 0, col = 0;
+            char buffer;
+            StreamReader fileVariable = new StreamReader(path);
+            do
+            {
+                buffer = (char)fileVariable.Read();
+                if (buffer == '\r') continue;
+                if (buffer == '\n')
+                {
+                    row++;
+                    col = 0;
+                }
+                else
+                {
+                    maze[rowsize, columnsize] = buffer;
+
+                    col++;
+                }
+            } while (!fileVariable.EndOfStream);
+            fileVariable.Close();
+        }
+        public Cell getcellleft(Cell c)
+        {
+            c.x = c.x - 1;
+            return c;
+        }
+        public Cell getcellright(Cell c)
+        {
+            c.x = c.x + 1;
+            return c;
+        }
+        public Cell getcellup(Cell c)
+        {
+            c.y = c.y - 1;
+            return c;
+        }
+        public Cell getcelldown(Cell c)
+        {
+            c.y = c.y +1;
+            return c;
+        }
+        public  Cell findpacman()
+        {
+            Pacman s = new Pacman();
+        }
+    }
+}
